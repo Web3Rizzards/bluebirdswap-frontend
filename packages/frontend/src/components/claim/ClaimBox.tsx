6fe79dcf45f2bf1fc3cdcbcccb156433f29fa1e4
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2023-03-04 12:45:34 -0700

    delete web5

--- packages/frontend/src/components/claim/ClaimBox.tsx
+++ packages/frontend/src/components/claim/ClaimBox.tsx
@@ -182,6 +182,7 @@ export const ClaimBox: FC = () => {
     borderRadius: '15px',
     width: '80%',
     gap: 2,
+    boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
   }
 
   const FlexStyle = {
