b6e36283526608c3e9e456001b494dcb3d685dd3
Phia(가영) <76558220+rkdud007@users.noreply.github.com>
2023-03-04 19:26:13 -0700

    landing

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -2,23 +2,45 @@ import { HomePageTitle } from '@components/home/HomePageTitle'
 import { HomeTopBar } from '@components/home/HomeTopBar'
 import { CenterBody } from '@components/layout/CenterBody'
 import type { NextPage } from 'next'
+import Image from 'next/image'
 import 'twin.macro'
+import { useAccount } from 'wagmi'
+
+import landing from 'public/images/landing-cover.svg'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Flex } from '@chakra-ui/react'
 
 const HomePage: NextPage = () => {
+  const buttonStyle = {
+    position: 'absolute',
+    top: '45%',
+    left: '42%',
+    width: '17em',
+    height: '4em',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+  const { isConnected } = useAccount()
   return (
     <>
-      {/* Top Bar */}
-      <HomeTopBar url="home" />
-      {/* <Image src="../landing.svg" width={10} height={10} /> */}
-
-      <CenterBody tw="mt-20 mb-10 px-5">
-        {/* Title */}
-        <HomePageTitle />
-        {/* Rainbowkit Connect Button */}
-        {/* <ConnectButton /> */}
-        {/* Greeter.sol Contract Interactions
-        <GreeterContractInteractions /> */}
-      </CenterBody>
+      {isConnected ? (
+        <>
+          {' '}
+          <HomeTopBar url="home" />
+          <CenterBody tw="mt-20 mb-10 px-5">
+            <HomePageTitle />
+          </CenterBody>{' '}
+        </>
+      ) : (
+        <CenterBody tw="px-5">
+          <Image src={landing} alt="pic" />
+          <Flex style={buttonStyle}>
+            <ConnectButton />
+          </Flex>
+        </CenterBody>
+      )}
     </>
   )
 }
